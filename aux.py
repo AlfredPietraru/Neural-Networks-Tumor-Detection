76a1102b3ca46a12ad0326790016dfc4db26ba1f
AlfredPietraru <alfred.andrei@yahoo.com>
2025-06-20 12:56:14 +0300

    started adding mlflow support

--- aux.py
+++ aux.py
@@ -3,8 +3,6 @@ import os
 import numpy as np
 import cv2
 from random import shuffle
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 
 TRAINING_DIR = "./Training"
@@ -94,97 +92,3 @@ def split_for_cross_validation(final_info: list[tuple], number_splits : int):
         final_info, chunk = split_traing_data(final_info, validation_split)
         chunk_values.append(chunk)
     return chunk_values
-
-
-def plot_data(training_loss, validation_loss, training_accuracy, validation_accuracy):
-  fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
-  sns.lineplot(x=range(1, len(training_loss) + 1), y=training_loss, ax=ax1, label='Training Loss', marker='o')
-  sns.lineplot(x=range(1, len(validation_loss) + 1), y=validation_loss, ax=ax1, label='Validation Loss', marker='o')
-  ax1.set_xlabel("Epoch")
-  ax1.set_ylabel("Loss")
-  ax1.set_title("Training and Validation Loss Over Epochs")
-  ax1.legend()
-
-  sns.lineplot(x=range(1, len(training_accuracy) + 1), y=training_accuracy, ax=ax2, label='Training Accuracy', marker='o')
-  sns.lineplot(x=range(1, len(validation_accuracy) + 1), y=validation_accuracy, ax=ax2, label='Validation Accuracy', marker='o')
-  ax2.set_xlabel("Epoch")
-  ax2.set_ylabel("Accuracy")
-  ax2.set_title("Training and Validation Accuracy Over Epochs")
-  ax2.legend()
-
-  plt.tight_layout()
-  plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#   import random
-# import functools
-
-# BEST_MODEL_NUMBER = 1
-# CONFIDENCE = 0.95
-
-# def parameter_sets_equal(param1, param2):
-#     for key in param1.keys():
-#         if param1[key] != param2[key]: return False
-#     return True
-
-# def compute_next_parameters(all_parameters):
-#     dictionary = {}
-#     for key in all_parameters.keys():
-#         dictionary[key] = random.choice(all_parameters[key])
-#     return dictionary
-
-# def nr_models_for_certainty(nr_models, nr_best_models):
-#     out = np.array([1.0])
-#     for i in range(nr_models):
-#         out *= (nr_models - nr_best_models - i) / nr_models
-#         if (1 - out > CONFIDENCE): return i
-#     return i
-
-# def all_models_paramters(all_parameters):
-#     max_nr_models = functools.reduce(lambda a, b: a * b, list(map(lambda x: len(x), all_parameters.values())))
-#     nr_models_neccesary = nr_models_for_certainty(max_nr_models, BEST_MODEL_NUMBER)
-#     print("There will be ", nr_models_neccesary, "for certainty of ", CONFIDENCE, " that we found the right one.")
-#     parameters_list = [compute_next_parameters(all_parameters)]
-#     for i in range(nr_models_neccesary - 1):
-#         while(1):
-#             current_param = compute_next_parameters(all_parameters)
-#             foundParameters = False
-#             for param in parameters_list:
-#                 if (not parameter_sets_equal(current_param, param)):
-#                     foundParameters = True
-#                     break
-#             if foundParameters:
-#                 parameters_list.append(current_param)
-#                 break
-#     return parameters_list
-
-# def compute_accuracy_for_all_models(all_parameters):
-#     models_parameters = all_models_paramters(all_parameters)
-#     best_accuracy = 0
-#     best_parameters = None
-#     for parameters in models_parameters:
-#         acc = compute_cross_validation(parameters)
-#         print(parameters, f"Test Accuracy: {acc:.2f}")
-#         if (acc > best_accuracy):
-#             best_accuracy = acc
-#             best_parameters = parameters
-#     return best_parameters, best_accuracy
-
-# best_parameter, best_accuracy = compute_accuracy_for_all_models(grid_parameters)
